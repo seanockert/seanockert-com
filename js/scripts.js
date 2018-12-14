9be2c79f58c4553d26a8ef402126d7fb549c5e7f
Sean Ockert <seanockert@gmail.com>
2018-12-14 11:03:38 +1000

    Disable Barba links on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,18 @@
 	}
 }*/
 
+var isMobile = function() {
+  if (window.innerWidth < 600 ||
+      (navigator.userAgent.match(/Android/i) && window.innerWidth < 980) || 
+      navigator.userAgent.match(/webOS/i) || 
+      navigator.userAgent.match(/iPhone/i) || 
+      navigator.userAgent.match(/BlackBerry/i) || 
+      navigator.userAgent.match(/Windows Phone/i)
+  ) {  
+    return true;
+  }
+  return false;
+} 
 
 // Load fonts
 (function(w){
@@ -16,70 +28,65 @@
   var bodyFont = new FontFaceObserver('Merriweather');
   var headerFont = new FontFaceObserver('Lato');
   Promise.all([bodyFont.load(), headerFont.load()]).then(function () {
-    console.log('Fonts loaded');
     w.document.documentElement.className += ' fonts-loaded';
   });
 }(this));
 
-Barba.Pjax.start();
-Barba.Prefetch.init();
+if (!isMobile) {
+  Barba.Pjax.start();
+  Barba.Prefetch.init();
 
-Barba.Dispatcher.on('initStateChange', function() {
-  var path = location.pathname;
-  //console.log(path);
-  if (path != '/') {
-    ga('send', 'pageview', path);
-    ga('send', 'event', 'pageview', path, '');
-  }
-});
-
-var delay = 900;
-
-var SlideTransition = Barba.BaseTransition.extend({
-  start: function() {
-	Promise.all([this.newContainerLoading, this.slideOut()]).then(this.slideIn.bind(this));
-  },
-
-  slideOut: function() {
-  	var elem = this.oldContainer;
-  	elem.classList.remove('transition-in');
-	elem.offsetWidth = elem.offsetWidth;
-	return elem.classList.add('transition-out');
-    //.promise();
-  },
-
-  slideIn: function() {
-    var _this = this;
-    var elem = _this.newContainer;
-    elem.classList.add('transition-in');
-    elem.style.visibility = 'visible';
-    //elem.style.opacity = 0;
-    window.scrollTo(0, 0);
-
-    if (window.innerWidth < 512) {
-    	delay = 300;	
-    } else {
-    	delay = 900;
+  Barba.Dispatcher.on('initStateChange', function() {
+    var path = location.pathname;
+    //console.log(path);
+    if (path != '/') {
+      ga('send', 'pageview', path);
+      ga('send', 'event', 'pageview', path, '');
     }
+  });
 
-    var waitTime = window.setTimeout(function() {
-    	_this.oldContainer.style.visibility = 'hidden';
-    	_this.done();
-    	window.clearTimeout(waitTime);
+
+  var delay = 900;
+
+  var SlideTransition = Barba.BaseTransition.extend({
+    start: function() {
+  	Promise.all([this.newContainerLoading, this.slideOut()]).then(this.slideIn.bind(this));
+    },
+
+    slideOut: function() {
+    	var elem = this.oldContainer;
     	elem.classList.remove('transition-in');
-		elem.offsetWidth = elem.offsetWidth;
-    }, delay) 
-  }
-});
+  	elem.offsetWidth = elem.offsetWidth;
+  	return elem.classList.add('transition-out');
+      //.promise();
+    },
+
+    slideIn: function() {
+      var _this = this;
+      var elem = _this.newContainer;
+      elem.classList.add('transition-in');
+      elem.style.visibility = 'visible';
+      //elem.style.opacity = 0;
+      window.scrollTo(0, 0);
+
+      if (window.innerWidth < 512) {
+      	delay = 300;	
+      } else {
+      	delay = 900;
+      }
+
+      var waitTime = window.setTimeout(function() {
+      	_this.oldContainer.style.visibility = 'hidden';
+      	_this.done();
+      	window.clearTimeout(waitTime);
+      	elem.classList.remove('transition-in');
+  		elem.offsetWidth = elem.offsetWidth;
+      }, delay) 
+    }
+  });
 
 
-Barba.Pjax.getTransition = function() {
-  return SlideTransition;
-};
-
-
-if ('addEventListener' in document) {
-    document.addEventListener('DOMContentLoaded', function() {
-        FastClick.attach(document.body);
-    }, false);
+  Barba.Pjax.getTransition = function() {
+    return SlideTransition;
+  };
 }
